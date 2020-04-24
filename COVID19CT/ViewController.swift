093335c30d06aec795e693b47311a7996729c2c5
javierbustillo <javier.bustillo@upr.edu>
2020-04-24 14:38:56 -0400

    Initial Commit

---
+++ COVID19CT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19CT
+//
+//  Created by Javier Bustillo on 4/24/20.
+//  Copyright © 2020 Javier Bustillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
